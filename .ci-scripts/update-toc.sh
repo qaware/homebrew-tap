7dafbc2a6e9a19bf54f472f323514938377943d9
Alexander Eimer <alexander.eimer@qaware.de>
2023-10-25 17:05:53 +0200

    fix: adapt to brew

--- .ci-scripts/update-toc.sh
+++ .ci-scripts/update-toc.sh
@@ -4,16 +4,20 @@ set -euo pipefail
 # Enable debug - print commands
 #set -x
 
-TAP_GEN_TMP_PATH=".parsed-formula"
-
 function generateToc() {
+  programNameList=()
+
   while read -r file; do
-    formulaName=${file//.yaml/}
-    1>&2 echo "  - $formulaName"
-    printf -- "* %s\n" "$formulaName"
-  done < <(find "$TAP_GEN_TMP_PATH" -maxdepth 1 -type f -name '*.yaml' -exec basename {} \; | sort)
+    # shellcheck disable=SC2001
+    programName=$(sed -E 's/(@.+)?\.rb//g' <<<"$file")
+    programNameList+=("$programName")
+  done < <(find "Formula" -maxdepth 1 -type f -name '*.rb' -exec basename {} \; | sort)
+
+  printf "* %s\n" "${programNameList[@]}" | sort -u
 }
 
 echo "Generating TOC in README.md"
+toc=$(generateToc)
+echo "$toc"
 # https://stackoverflow.com/questions/2699666/replace-delimited-block-of-text-in-file-with-the-contents-of-another-file
-sed -i -ne '/<!-- BEGIN TOC -->/ {p; r '<(generateToc) -e ':a; n; /<!-- END TOC -->/ {p; b}; ba}; p' README.md
+sed -i -ne '/<!-- BEGIN TOC -->/ {p; r '<<<"$toc" -e ':a; n; /<!-- END TOC -->/ {p; b}; ba}; p' README.md
