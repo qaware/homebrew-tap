186df4a3e8cad2fdfd08fbf3b5c4e75b8354c9a6
Alexander Eimer <alexander.eimer@qaware.de>
2023-10-25 17:10:17 +0200

    fix: push

--- .ci-scripts/commit-push.sh
+++ .ci-scripts/commit-push.sh
@@ -9,4 +9,4 @@ git commit --quiet -m "chore: update Aliases" || {
   echo "Nothing to commit or commit failed, will exit now"
   exit 0
 }
-git push "https://oauth2:${REPO_WRITE_TOKEN}@${CI_SERVER_HOST}/${CI_PROJECT_PATH}.git" "HEAD:$CI_COMMIT_BRANCH"
+git push
