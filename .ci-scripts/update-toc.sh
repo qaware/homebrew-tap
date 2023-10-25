03c3b801d67219749cad8c360a8fe463d0816473
Alexander Eimer <alexander.eimer@qaware.de>
2023-10-25 12:23:55 +0200

    fix: update toc

--- .ci-scripts/update-toc.sh
+++ .ci-scripts/update-toc.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 # Enable debug - print commands
 #set -x
 
+TAP_GEN_TMP_PATH=".parsed-formula"
+
 function generateToc() {
   while read -r file; do
     formulaName=${file//.yaml/}
